7ad60d27cca19bc50ae9e7c7bd49c86cdf624792
lidian <837997288@qq.com>
2023-05-17 21:14:06 +0800

    修改fasttext训练失效，保存模型大小异常

--- train_fasttext.py
+++ train_fasttext.py
@@ -4,15 +4,7 @@ import json
 import os
 import pickle
 from typing import List
-# print(common_texts[0])
-# min_count = 1
-# extra_words = [['<PAD>' for _ in range(min_count)] + ['<START>' for _ in range(min_count)] +\
-#                    ['<EOS>' for _ in range(min_count)] + ['<UNK>' for _ in range(min_count)]]
-# model = FastText(size=128, window=5, min_count=min_count)  # instantiate
-# model.build_vocab(sentences=extra_words)
-# model.build_vocab(sentences=common_texts, update=True)
-# model.train(sentences=common_texts, total_examples=len(common_texts), epochs=10)
-# print(model.wv['human'])
+
 
 def is_alpha(string):
     for x in string:
@@ -22,55 +14,59 @@ def is_alpha(string):
             return False
     return True
 
+import json
+
 def json2corpus(file_path):
     res = []
-    with open(file_path,'r', encoding='utf8') as f:
+    with open(file_path, 'r', encoding='utf8') as f:
         for method_json in f.readlines():
             try:
-                fst_part, sec_part = method_json.split(', ')
-                token, test_name = json.loads(fst_part), json.loads(sec_part)
-                # method_json = json.loads(method_json)
-            except:
-                continue
-            # content = method_json[0]
-            # name = method_json[1]
-            cur_line = token + test_name
+                # 解析JSON数据
+                method_data = json.loads(method_json)
+                # 获取方法体中的所有元素，除了最后一个元素
+                tokens = [token[0] for token in method_data[:-1]]
+                # 获取最后一个元素作为方法名
+                method_name = method_data[-1]
+                # 创建当前行
+                cur_line = tokens + method_name
 
-            # cur_line = method_json[-1]
-            # # cur_line = method_json[-1] + method_json[-2]  # only for kuis dataset
-            # for x in method_json[:-1]:
-            # # for x in method_json[:-2]:  # only for kuis dataset
-            #     if is_alpha(x[0]):
-            #         cur_line.append(x[0])
-            res.append(cur_line)
+                # 将当前行添加到结果列表
+                res.append(cur_line)
+                # print(cur_line)
+            except:
+                print("error")
+                continue
     return res
+
+
 
 
 def save_vocab_weight(model, output_dir):
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
-    with open(os.path.join(output_dir, 'vocab.pkl'), 'wb') as f_vocab, open(os.path.join(output_dir,'weight.pkl'),'wb') as f_weight:
-            pickle.dump(model.wv.index2word, f_vocab)
-            pickle.dump(model.wv.vectors_vocab[:len(model.wv.index2word), :], f_weight)
+    with open(os.path.join(output_dir, 'vocab.pkl'), 'wb') as f_vocab, open(os.path.join(output_dir, 'weight.pkl'), 'wb') as f_weight:
+        pickle.dump(model.wv.index_to_key, f_vocab)
+        pickle.dump(model.wv[model.wv.index_to_key], f_weight)
 
-def main(corpus_path:List[str], output_dir):
-    extra_words = [['<PAD>' for _ in range(min_count)] + ['<START>' for _ in range(min_count)] +\
+
+def main(corpus_path: List[str], output_dir):
+    extra_words = [['<PAD>' for _ in range(min_count)] + ['<START>' for _ in range(min_count)] + \
                    ['<EOS>' for _ in range(min_count)] + ['<UNK>' for _ in range(min_count)]]
-    model = FastText(size=v_dim, window=window, min_count=min_count)  # instantiate
-    model.build_vocab(sentences=extra_words)
+    model = FastText(vector_size=v_dim, window=window, min_count=min_count)  # 实例化
+    model.build_vocab(corpus_iterable=extra_words)
     corpus = []
     for cp in corpus_path:
         corpus += json2corpus(cp)
-    model.build_vocab(sentences=corpus, update=True)
-    # model.train(sentences=corpus, total_examples=len(corpus), epochs=5)
+
+    model.build_vocab(corpus_iterable=corpus, update=True)
+    model.train(corpus_iterable=corpus, total_examples=len(corpus), epochs=10)
     save_vocab_weight(model, output_dir)
 
 if __name__ == '__main__':
     v_dim = 128
     window = 8
     min_count = 1
-    main(['train.json',
-          'validation.json',
-          'test.json',
-          ], './dataset/fasttext_vectors')
 
+    main(['.\\med_test\\train.json',
+          '.\\med_test\\valid.json',
+          '.\\med_test\\test.json'], './dataset/fasttext_vectors')
